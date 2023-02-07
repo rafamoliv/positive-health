808a796cc20e1bbefb098da833b24c797d94e709
rafael.oliveira <rafael.oliveira@psafe.com>
2023-02-07 15:11:16 -0300

    feat: create system page template for internal pages

--- src/templates/SystemPage/SystemPage.tsx
+++ src/templates/SystemPage/SystemPage.tsx
@@ -1,8 +1,18 @@
+import {
+  faRightFromBracket,
+  faCircleInfo,
+  faGrip,
+  faCalendarDays,
+  faListCheck,
+  faGear
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
 import { ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-import { IconLogoPh, IconLogoPositiveHealth } from '@/assets'
-import { privateURL } from '@/routes/urls'
+import { IconLogoPh } from '@/assets'
+import { privateURL, publicURL } from '@/routes/urls'
 
 /**
  * Internal pages template
@@ -14,13 +24,20 @@ interface SystemPageProps {
 }
 
 export const SystemPage = ({ children }: SystemPageProps) => {
+  const { pathname: location } = useLocation()
+
+  const labelStyles = 'fw-semibold'
+  const linkStyles = 'd-flex flex-column text-center text-decoration-none p-2'
+  const listStyles = 'mb-2'
+  const svgStyles = 'fs-4'
+
   return (
-    <div className="d-flex flex-nowrap vh-100">
-      <div
-        className="d-flex flex-column flex-shrink-0 bg-dark"
-        style={{ width: '4.5rem' }}
+    <div className="bg-light d-flex flex-nowrap vh-100 w-100">
+      <nav
+        className="d-flex flex-column flex-shrink-0 pt-3 pb-3 bg-dark"
+        style={{ width: '5rem' }}
       >
-        <Link to={privateURL.DASHBOARD}>
+        <Link className={linkStyles} to={privateURL.DASHBOARD}>
           <figure className="text-center">
             <img
               alt="PositiveHealth logo small"
@@ -29,16 +46,75 @@ export const SystemPage = ({ children }: SystemPageProps) => {
             />
           </figure>
         </Link>
-        teste
-      </div>
-      <div>
-        <nav>
-          <figure>
-            <img alt="PositiveHealth Logo" src={IconLogoPositiveHealth} />
-          </figure>
-        </nav>
-        <p>sidebar</p>
-        {children}
+
+        <ul className="mb-auto p-0">
+          <li className={listStyles}>
+            <Link
+              className={clsx(linkStyles, {
+                'link-success': location === privateURL.DASHBOARD
+              })}
+              to={privateURL.DASHBOARD}
+            >
+              <FontAwesomeIcon className={svgStyles} icon={faGrip} />
+              <span className={labelStyles}>Painel</span>
+            </Link>
+          </li>
+          <li className={listStyles}>
+            <Link
+              className={clsx(linkStyles, {
+                'link-success': location === privateURL.SCHEDULING
+              })}
+              to={privateURL.SCHEDULING}
+            >
+              <FontAwesomeIcon className={svgStyles} icon={faCalendarDays} />
+              <span className={labelStyles}>Agenda</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={clsx(linkStyles, {
+                'link-success': location === privateURL.CONSULTATION
+              })}
+              to={privateURL.CONSULTATION}
+            >
+              <FontAwesomeIcon className={svgStyles} icon={faListCheck} />
+              <span className={labelStyles}>Consulta</span>
+            </Link>
+          </li>
+        </ul>
+
+        <Link className={linkStyles} to={privateURL.DASHBOARD}>
+          <FontAwesomeIcon className={svgStyles} icon={faGear} />
+        </Link>
+
+        <Link
+          className={linkStyles}
+          target={'_blank'}
+          to={'https://www.linkedin.com/in/rafamoliv/'}
+        >
+          <FontAwesomeIcon className={svgStyles} icon={faCircleInfo} />
+        </Link>
+
+        <Link className={linkStyles} to={publicURL.SIGNIN}>
+          <FontAwesomeIcon className={svgStyles} icon={faRightFromBracket} />
+        </Link>
+      </nav>
+
+      <div className="align-items-center d-flex flex-column w-100 p-3">
+        <header className="pl-2 pr-2">
+          <div
+            className="d-flex align-items-center justify-content-between"
+            style={{ width: '1400px' }}
+          >
+            <h2>Olá, Rafael Oliveira</h2>
+
+            <figure>
+              <img alt="PositiveHealth Logo" src={'https://picsum.photos/80'} />
+            </figure>
+          </div>
+        </header>
+
+        <section style={{ width: '1400px' }}>{children}</section>
       </div>
     </div>
   )
