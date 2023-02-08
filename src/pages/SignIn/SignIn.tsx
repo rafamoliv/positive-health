dc1126ebcb0cc0f68587ec160b132028d3be3664
rafael.oliveira <rafael.oliveira@psafe.com>
2023-02-08 13:04:27 -0300

    feat: add react hook form and yup in signin

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,15 +1,37 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useContext, useEffect } from 'react'
+import { Figure } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
+import * as yup from 'yup'
+
+import { Form } from '@/components'
 
 import { IconLogoPositiveHealth } from '@/assets'
 import { AppContext } from '@/context/AppContext'
 import { privateURL } from '@/routes/urls'
+import { SignPage } from '@/templates'
+
+export interface SignInFormData {
+  email: string
+  password: string
+}
+
+const sigInFormSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).max(16).required()
+})
 
 const SignIn = () => {
   const { user, handleSignIn } = useContext(AppContext)
   const navigate = useNavigate()
 
-  const handleSetSignIn = (email: string) => {
+  const { register, handleSubmit } = useForm<SignInFormData>({
+    mode: 'onChange',
+    resolver: yupResolver(sigInFormSchema)
+  })
+
+  const onSubmit = ({ email }: { email: string }) => {
     handleSignIn?.(email)
   }
 
@@ -19,13 +41,30 @@ const SignIn = () => {
   }, [user])
 
   return (
-    <div>
-      Login
-      <figure>
-        <img alt="PositiveHealth Logo" src={IconLogoPositiveHealth} />
-      </figure>
-      <button onClick={() => handleSetSignIn('teste@te.com')}>Entrar</button>
-    </div>
+    <SignPage>
+      <Figure>
+        <Figure.Image
+          alt="PositiveHealth Logo"
+          src={IconLogoPositiveHealth}
+          width={360}
+        />
+      </Figure>
+      <Form.Root onSubmit={handleSubmit(onSubmit)}>
+        <Form.Input
+          id={'email'}
+          placeholder="Email"
+          register={register}
+          type="text"
+        />
+        <Form.Input
+          id={'password'}
+          placeholder="Password"
+          register={register}
+          type="password"
+        />
+        <Form.Button>Sign In</Form.Button>
+      </Form.Root>
+    </SignPage>
   )
 }
 
