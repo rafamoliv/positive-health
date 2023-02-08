37f46121d9e9632ba5a4fe63c98a2921acb2b046
rafael.oliveira <rafael.oliveira@psafe.com>
2023-02-08 00:30:34 -0300

    feat: finishing profile modal

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,17 @@
+import { faker } from '@faker-js/faker'
+import { useState } from 'react'
 import { Figure } from 'react-bootstrap'
 
-import { Button } from '@/components/Button'
-import { Calendar } from '@/components/Calendar'
-import { Card } from '@/components/Card'
+import { Button, Calendar, Card, Label, Modal } from '@/components'
 
 import config from './Dashboard.config'
-import { LineChart } from './charts/LineChart'
-import { PieChart } from './charts/PieChart'
+import { LineChart, PieChart } from './charts'
 
 import { SystemPage } from '@/templates/SystemPage'
 
 const Dashboard = () => {
+  const [showPatientModal, setShowPatientModal] = useState(false)
+
   return (
     <SystemPage.Root>
       <SystemPage.Section>
@@ -46,11 +47,44 @@ const Dashboard = () => {
                   </Figure>
                   <span>{x.name}</span>
                 </div>
-                <Button>View profile</Button>
+                <Button onClick={() => setShowPatientModal(true)}>
+                  View profile
+                </Button>
               </div>
             </Card.Item>
           ))}
         </Card.Root>
+
+        <Modal
+          onHide={() => setShowPatientModal(false)}
+          show={showPatientModal}
+          title={'Patient'}
+        >
+          <div className="d-flex justify-content-between align-items-center">
+            <div>
+              <Label title={'Id'}>{faker.datatype.uuid()}</Label>
+              <Label
+                title={'Fullname'}
+              >{`${faker.name.firstName()} ${faker.name.lastName()}`}</Label>
+            </div>
+            <Figure className="m-0 rounded-2">
+              <Figure.Image
+                alt={`${faker.name.firstName()} avatar`}
+                className="mb-0"
+                height={136}
+                rounded
+                src={faker.image.avatar()}
+                width={136}
+              />
+            </Figure>
+          </div>
+          <Label title={'Email'}>{faker.internet.email()}</Label>
+          <Label title={'Address'}>{faker.address.streetAddress()}</Label>
+          <Label title={'Gender'}>{faker.name.sexType()}</Label>
+          <Label title={'Plan'}>
+            {faker.helpers.arrayElement(['free', 'basic', 'business'])}
+          </Label>
+        </Modal>
       </SystemPage.Aside>
     </SystemPage.Root>
   )
