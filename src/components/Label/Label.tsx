842186114762902d89cc027b0a4c29ab8f1b2a41
rafael.oliveira <rafael.oliveira@psafe.com>
2023-02-08 00:29:58 -0300

    feat: create label component

---
+++ src/components/Label/Label.tsx
@@ -0,0 +1,15 @@
+import { ReactNode, FC, HTMLAttributes } from 'react'
+
+interface LabelProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode
+  title: string
+}
+
+export const Label: FC<LabelProps> = ({ children, title }, rest) => {
+  return (
+    <div {...rest}>
+      <label className="fs-6">{title}</label>
+      <p className="fs-4 fw-semibold">{children}</p>
+    </div>
+  )
+}
