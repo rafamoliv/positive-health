40bb0a46065c80fad657efc40e4100e4fdc04d16
rafael.oliveira <rafael.oliveira@psafe.com>
2023-02-07 21:15:39 -0300

    fix: reorganize dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,61 +1,51 @@
-import * as React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip } from 'chart.js'
+import { Doughnut } from 'react-chartjs-2'
 import { useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
 
-import { Button } from '@/components'
+import { Calendar } from '@/components/Calendar'
+import { Card } from '@/components/Card'
 
-import { reactLogo } from '@/assets/icons'
 import { useFetchReposQuery } from '@/services/api'
 import { SystemPage } from '@/templates/SystemPage'
 
+ChartJS.register(ArcElement, Tooltip)
+
 const Dashboard = () => {
-  const dispatch = useDispatch()
   const { t } = useTranslation('pgHome')
-  const { data = [] } = useFetchReposQuery()
-  const [count, setCount] = React.useState(0)
+  const { data = [] } = useFetchReposQuery('')
+
+  const dataChart = {
+    labels: ['Atendidos', 'Pendente de atendimento'],
+    datasets: [
+      {
+        data: [12, 19],
+        backgroundColor: ['rgb(119, 120, 135)', 'rgb(121, 150, 155)']
+      }
+    ]
+  }
 
   return (
-    <SystemPage>
-      <div>
+    <SystemPage.Root>
+      <SystemPage.Section>
         <div>
-          <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-            <img alt="Vite logo" src="/vite.svg" />
-          </a>
-          <a href="https://reactjs.org" rel="noreferrer" target="_blank">
-            <img alt="React logo" src={reactLogo} />
-          </a>
-        </div>
+          <Card title={'Consultas agendadas'}>
+            <Doughnut data={dataChart} />
+          </Card>
 
-        <h1>{t('title')}</h1>
-        <p>{t('summary')}</p>
-
-        <div>
-          <Button onClick={() => setCount((count) => count + 1)} primary>
-            {t('count', { value: count })}
-          </Button>
+          <div>
+            <p>
+              <strong>{t('redux')}</strong>
+            </p>
+            {data?.map((x, i) => {
+              return <p key={i}>{x.name}</p>
+            })}
+          </div>
         </div>
-
-        <div>
-          <Button
-            onClick={() => {
-              alert(`Clicked! Change isInitialized to: ${'!isInitialized'}`)
-            }}
-            primary
-          >
-            {t('btn', { context: 'redux' })}
-          </Button>
-        </div>
-
-        <div>
-          <p>
-            <strong>{t('redux')}</strong>
-          </p>
-          {data?.map((x, i) => {
-            return <p key={i}>{x.name}</p>
-          })}
-        </div>
-      </div>
-    </SystemPage>
+      </SystemPage.Section>
+      <SystemPage.Aside>
+        <Calendar />
+      </SystemPage.Aside>
+    </SystemPage.Root>
   )
 }
 
