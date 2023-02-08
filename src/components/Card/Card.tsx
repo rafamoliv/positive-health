a90d51c2c4e7db98feb9d822b095f541faec63b0
rafael.oliveira <rafael.oliveira@psafe.com>
2023-02-07 23:05:47 -0300

    fix: fixing card component for multiples items

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,14 +2,26 @@ import { ReactNode } from 'react'
 
 interface CardProps {
   children: ReactNode
+}
+
+interface RootProps extends CardProps {
   title: string
 }
 
-export const Card = ({ children, title }: CardProps) => {
+export const Root = ({ children, title }: RootProps) => {
   return (
     <div>
       <h4>{title}</h4>
-      <div className="bg-white rounded-5 p-4">{children}</div>
+      <div className="d-flex flex-column gap-2">{children}</div>
     </div>
   )
+}
+
+export const Item = ({ children }: CardProps) => {
+  return <div className="bg-white rounded-5 p-4">{children}</div>
+}
+
+export const Card = {
+  Root,
+  Item
 }
